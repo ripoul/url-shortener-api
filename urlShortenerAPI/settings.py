5edc996e73de1e70633bb56e1247b6dc3dee4883
Jules LE BRIS <jls.lebris@gmail.com>
2019-07-11 22:55:10 +0200

    resolve conflict heroku vs travis

--- urlShortenerAPI/settings.py
+++ urlShortenerAPI/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -115,4 +114,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-django_heroku.settings(locals())
+try:
+    import django_heroku
+    django_heroku.settings(locals())
