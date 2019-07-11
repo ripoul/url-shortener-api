d979c6fc43c7660bf78cea5ead6c391a2cd1db3c
Jules LE BRIS <jls.lebris@gmail.com>
2019-07-11 18:07:27 +0200

    black format

--- api/views.py
+++ api/views.py
@@ -3,12 +3,13 @@ import requests
 from django.http import HttpResponse
 import json
 
+
 def tinyurl(request):
-    url = request.GET.get('url', '')
+    url = request.GET.get("url", "")
     if url:
-        payload = {'url': url}
+        payload = {"url": url}
         r = requests.get("http://tinyurl.com/api-create.php", params=payload)
-        ret = json.dumps({'url': r.text})
+        ret = json.dumps({"url": r.text})
         return HttpResponse(ret)
     else:
         ret = json.dumps({"error": "url parameter missing"})
