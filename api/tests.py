6e3f49f12cd5d1d5b81f2b4e6dea07a0cee3dea2
Jules LE BRIS <jls.lebris@gmail.com>
2019-07-11 23:02:18 +0200

    move test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import json
 
-# Create your tests here.
+
+class tinyurlCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_url_contains_tinyurl(self):
+        c = Client()
+        response = c.get("/api/tinyurl", {"url": "https://www.google.fr"})
+        url = response.json()["url"]
+        self.assertTrue("tinyurl" in url)
+
+    def test_if_no_param(self):
+        c = Client()
+        response = c.get("/api/tinyurl")
+        self.assertEqual(response.status_code, 400, "unexpected return code")
