c5aba67aa9cf3ca34164630632e663338b45b83b
Jules LE BRIS <jls.lebris@gmail.com>
2019-07-16 14:10:04 +0200

    add rebrandly view

--- api/urls.py
+++ api/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("tinyurl", views.tinyurl, name="tinyurl")]
+urlpatterns = [path("tinyurl", views.tinyurl, name="tinyurl"),path("rebrandly", views.rebrandly, name="rebrandly")]
--- api/views.py
+++ api/views.py
@@ -14,3 +14,39 @@ def tinyurl(request):
     else:
         ret = json.dumps({"error": "url parameter missing"})
         return HttpResponse(ret, status=400, content_type="application/json")
+
+
+def rebrandly(request):
+    url = request.GET.get("url", "")
+    if url:
+        linkRequest = {
+            "destination": url,
+            "domain": {"fullName": "rebrand.ly"}
+            # , "slashtag": "A_NEW_SLASHTAG"
+            # , "title": "Rebrandly YouTube channel"
+        }
+
+
+        requestHeaders = {
+            "Content-type": "application/json",
+            "apikey": "7b9459064585418cbcb1fc2538e2d2a9",
+            "workspace": "3e8f5fd7ef014e58b9f605737d91c750",
+        }
+
+        r = requests.post(
+            "https://api.rebrandly.com/v1/links",
+            data=json.dumps(linkRequest),
+            headers=requestHeaders,
+        )
+
+        if r.status_code == requests.codes.ok:
+            link = r.json()
+            print("Long URL was %s, short URL is %s" % (link["destination"], link["shortUrl"]))
+            ret = json.dumps({"url": link["shortUrl"]})
+            return HttpResponse(ret, content_type="application/json")
+        else:
+            ret = json.dumps({"error": "error with the request to the rebrandly api", "detail": r.text})
+            return HttpResponse(ret, status=400, content_type="application/json")
+    else:
+        ret = json.dumps({"error": "url parameter missing"})
+        return HttpResponse(ret, status=400, content_type="application/json")
