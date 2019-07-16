7007a1105a828acee454e93787c2879c35aed13c
Jules LE BRIS <jls.lebris@gmail.com>
2019-07-16 12:58:36 +0200

    fix #2

--- urlShortenerAPI/settings.py
+++ urlShortenerAPI/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "14&d%!!gv3#nx*vhp=djuhhqazpafgovcnu6z=p_3!_^70u66p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", True)
+
 
 ALLOWED_HOSTS = []
 
