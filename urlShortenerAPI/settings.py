1a24181904dbdaa6f33e19bba2ad7ea787312444
Jules LE BRIS <jls.lebris@gmail.com>
2019-07-18 13:19:49 +0200

    Update settings.py

--- urlShortenerAPI/settings.py
+++ urlShortenerAPI/settings.py
@@ -31,11 +31,12 @@ DEBUG = distutils.util.strtobool(os.getenv("DEBUG", "True"))
 
 
 ALLOWED_HOSTS = ["*"]
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
