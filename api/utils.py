8e58ee5dba680622dab90c3789a414b437889805
Jules LE BRIS <jls.lebris@gmail.com>
2019-09-01 15:44:01 +0200

    working firestore version

--- api/utils.py
+++ api/utils.py
@@ -6,7 +6,8 @@ import os
 def get_vars(name):
     try:
         db = firestore.Client()
-        env_vars = db.collection(u"env_vars").document(u"z4o3OFwXqFRGs1BvHfwk")
-        return env_vars[name]
+        doc_ref = db.collection(u"env_vars").document(u"z4o3OFwXqFRGs1BvHfwk")
+        doc = doc_ref.get().to_dict()
+        return doc[name]
     except DefaultCredentialsError:
         return os.getenv(name)
