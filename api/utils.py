181d7a8d7ad4523be06c509e6c81e4c987c6d0e1
Jules LE BRIS <jls.lebris@gmail.com>
2019-09-02 08:42:12 +0200

    lint

--- api/utils.py
+++ api/utils.py
@@ -3,7 +3,7 @@ import os
 
 
 def get_vars(name):
-    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+    if os.getenv("SERVER_SOFTWARE", "").startswith("Google App Engine/"):
         db = firestore.Client()
         doc_ref = db.collection(u"env_vars").document(u"z4o3OFwXqFRGs1BvHfwk")
         doc = doc_ref.get().to_dict()
