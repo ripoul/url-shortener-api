6b1fce300b4c70c5bbbfbd9d8687da0924c49bd3
Jules LE BRIS <jls.lebris@gmail.com>
2019-09-02 08:40:14 +0200

    change detection of plateforme

--- api/utils.py
+++ api/utils.py
@@ -1,13 +1,12 @@
 from google.cloud import firestore
-from google.auth.exceptions import DefaultCredentialsError
 import os
 
 
 def get_vars(name):
-    try:
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
         db = firestore.Client()
         doc_ref = db.collection(u"env_vars").document(u"z4o3OFwXqFRGs1BvHfwk")
         doc = doc_ref.get().to_dict()
         return doc[name]
-    except DefaultCredentialsError:
+    else:
         return os.getenv(name)
