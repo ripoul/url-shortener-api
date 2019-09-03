1a6d8da0a1c8a8a44731574fb6bac24e9f0f6ea7
Jules LE BRIS <jls.lebris@gmail.com>
2019-09-03 18:55:26 +0200

    Gcloud migration (#55)
    * first shot
    
    * change python version
    
    * working gcoud env
    :warning: missing env vars
    
    * black lint
    
    * add firestore for env vars store
    
    * update req
    
    * working firestore version
    
    * change detection of plateforme
    
    * lint
    
    * remove useless else
    
    * update travis build

---
+++ api/utils.py
@@ -0,0 +1,11 @@
+from google.cloud import firestore
+import os
+
+
+def get_vars(name):
+    if os.getenv("SERVER_SOFTWARE", "").startswith("Google App Engine/"):
+        db = firestore.Client()
+        doc_ref = db.collection(u"env_vars").document(u"z4o3OFwXqFRGs1BvHfwk")
+        doc = doc_ref.get().to_dict()
+        return doc[name]
+    return os.getenv(name)
