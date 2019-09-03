123bb2e6b5a9645f25d1a45a3cbf5326169f5ada
Jules LE BRIS <jls.lebris@gmail.com>
2019-09-03 21:02:11 +0200

    try

--- urlShortenerAPI/settings.py
+++ urlShortenerAPI/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.getenv("SERVER_SOFTWARE", "").startswith("gunicorn"):
-    SECURE_PROXY_SSL_HEADER = ("X-FORWARDED-PROTO", "HTTPS")
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     DEBUG = False
 
 
