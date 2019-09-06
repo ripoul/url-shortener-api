4e8d93b42c6552d12e6537a4900b5d9812eb325f
Jules LE BRIS <jls.lebris@gmail.com>
2019-09-06 11:23:20 +0200

    Update utils.py

--- api/utils.py
+++ api/utils.py
@@ -5,7 +5,7 @@ import os
 def get_vars(name):
     if os.getenv("SERVER_SOFTWARE", "").startswith("Google App Engine/"):
         db = firestore.Client()
-        doc_ref = db.collection(u"env_vars").document(u"z4o3OFwXqFRGs1BvHfwk")
+        doc_ref = db.collection(u"env_vars").document(u"env_prod")
         doc = doc_ref.get().to_dict()
         return doc[name]
     return os.getenv(name)
